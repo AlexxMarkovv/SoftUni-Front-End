4b16c5f22e300fbd86984a9643dee728406015bf
Alex Markov <langhorne05@gmail.com>
2024-12-24 22:25:59 +0200

    Create task05.js

---
+++ 03 JS Syntax Fundamentals LAB/task05.js
@@ -0,0 +1,28 @@
+function solve(num1, num2, operator) {
+
+    let result = undefined;
+
+    if (operator === '+') {
+        result = num1 + num2;
+    }
+    else if (operator === '-') {
+        result = num1 - num2;
+    }
+    else if (operator === '*') {
+        result = num1 * num2;
+    }
+    else if (operator === '/') {
+        result = num1 / num2;
+    }
+    else if (operator === '%') {
+        result = num1 % num2;
+    }
+    else if (operator === '**') {
+        result = num1 ** num2;
+    }
+
+    console.log(result);
+}
+
+solve(5, 6, '+')
+solve(3, 5.5, '**')
